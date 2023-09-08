ab58905d75cd6ec02ea4f4966edfa0d44e86223b
VinoMurugan <vinothini.jit@gmail.com>
2023-09-08 17:11:11 -0400

    Add new folder and files

--- src/component/AstronomyPicture.js
+++ src/component/AstronomyPicture.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
-export default function AstronmyPicture ()  {
-  const apiKey = 'rwhCGfRfZ8AOd3uHhsC8e8kjYYfzLca';
- 
- const [nasaData,setnasaDdata] = useState(null);
+const AstronomyPicture = ({ apiKey, date }) => {
+  const [apodData, setApodData] = useState(null);
 
- const getPicture = async (date) =>{
-  const response = await fetch (
-    `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`
-  )
-  const data = await response.json();
-  setnasaDdata(data)
- }
-useEffect(()=>{
-  getPicture('It')
-},[])
-return (
+  useEffect(() => {
+    const fetchAPOD = async () => {
+      try {
+        const response = await fetch(
+          `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`
+        );
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setApodData(data);
+      } catch (error) {
+        console.error('Error fetching APOD:', error);
+      }
+    };
 
     fetchAPOD();
   }, [apiKey, date]);
